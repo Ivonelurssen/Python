ac46d3ac0981e02a40705c708a4daea8f355592b
Ivone Lurssen <ivonesical@icloud.com>
2022-10-26 21:46:28 -0400

    Python

---
+++ for_loops.py
@@ -0,0 +1,25 @@
+for i in range(151):
+    print(i)
+
+for i in range (5, 1005, 5):
+    print(i)
+
+for i in range(1,101):
+    print(i)
+    if i%5==0:
+        print("Coding Dojo")
+
+x=1
+y=500000
+total=sum(range(x,y))
+print(total)
+
+for i in range(2018,0,-4):
+    print(i)
+
+lowNum=2
+highNum=9
+mult=3
+for i in range(lowNum, highNum+1):
+    if i%mult ==0:
+        print(i)
